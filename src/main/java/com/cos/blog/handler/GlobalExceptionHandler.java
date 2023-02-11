8c90d8878cc0e3a3627a483b8544bca612b06e99
JJongho123 <dam33534@naver.com>
2023-02-11 17:08:00 +0900

    시큐리티 구현1

--- src/main/java/com/cos/blog/handler/GlobalExceptionHandler.java
+++ src/main/java/com/cos/blog/handler/GlobalExceptionHandler.java
@@ -1,8 +1,11 @@
 package com.cos.blog.handler;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.cos.blog.dto.ResponseDto;
 
 @ControllerAdvice //모든 에러가 발생시 현재 페이지로 와서 처리를 하게해줌
 @RestController
@@ -11,8 +14,9 @@ public class GlobalExceptionHandler {
 	// value 에 해당하는 에러에 대해서만 해당방식으로 처리 
 	// ( 최상위 Exception으로 적용하면 모든 에러가 해당방식으로 처리됨)
 	@ExceptionHandler(value=IllegalArgumentException.class) 
-	public String handleArgumentException(IllegalArgumentException e) {
-		return "<h1>" +e.getMessage()+"</h1>";
+	public ResponseDto<String> handleArgumentException(IllegalArgumentException e) {
+		
+		return new ResponseDto<String>(HttpStatus.INTERNAL_SERVER_ERROR.value(), e.getMessage());
 	}
 
 }
--- src/main/webapp/WEB-INF/views/user/joinForm.jsp
+++ src/main/webapp/WEB-INF/views/user/joinForm.jsp
@@ -21,27 +21,7 @@
 
 </div>
 
-<script>
-let index = {
-		init: function(){
-			$("#btn-save").on("click",()=>{
-				this.save();
-			});
-		},
-		save: function(){
-			let data = {
-					username: $("#username").val(),
-					password: $("#password").val(),
-					email: $("#email").val()
-			}
-			//console.log(data);
-			
-			$.ajax().done().fail(); // ajax 통신을 이용해서 3개의 데이터를 json으로 변경하여 insert 요청
-			
-		}
-	}
-index.init();
+<script src="/js/user.js"></script>
 
-</script>
 <%@ include file="../layout/footer.jsp"%>
 
