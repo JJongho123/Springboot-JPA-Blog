0066a05b4fc6fb985324ece24929864616486dcf
JJongho123 <dam33534@naver.com>
2023-02-12 19:28:04 +0900

    글삭제기능  구현완료

--- src/main/resources/static/js/board.js
+++ src/main/resources/static/js/board.js
@@ -1,7 +1,10 @@
 let index = {
 	init: function() {
-		$("#btn-save").on("click", () => { //function(){}, ()=>{} this를 바인딜하기 위해서 !!
+		$("#btn-save").on("click", () => {
 			this.save();
+		});
+		$("#btn-delete").on("click", () => { 
+			this.deleteById();
 		});
 	},
 	save: function() {
@@ -25,6 +28,25 @@ let index = {
 			alert(JSON.stringify(error));
 
 		}); // ajax 통신을 이용해서 3개의 데이터를 json으로 변경하여 insert 요청
+	
+	},
+	deleteById: function() {
+		var id = $("#id").text();
+		
+		$.ajax({
+			type: "DELETE",
+			url: "/api/board/"+id,
+			dataType: "json"  // 요청을 서버로해서 응답이 왔을 때 기본적으로 모든 것이문자열(생긴게 json이라면) => javascript 오브젝트로 변경
+			 
+		}).done(function(resp) {
+			console.log(resp);
+			alert("삭제가 완료 되었습니다.");
+			location.href = "/";
+
+		}).fail(function(error) {
+			alert(JSON.stringify(error));
+
+		}); 
 	
 	}
 }
