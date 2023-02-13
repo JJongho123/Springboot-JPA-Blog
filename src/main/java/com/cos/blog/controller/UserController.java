09c5e3636ccdd6808c5d37157f2069bf3c7d6b1b
JJongho123 <dam33534@naver.com>
2023-02-13 18:01:35 +0900

    회정수정[2]완료

--- src/main/java/com/cos/blog/controller/UserController.java
+++ src/main/java/com/cos/blog/controller/UserController.java
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
+	
+	
 
 	@GetMapping("/auth/joinForm")
 	public String joinForm() {
--- src/main/java/com/cos/blog/controller/api/UserApiController.java
+++ src/main/java/com/cos/blog/controller/api/UserApiController.java
@@ -2,7 +2,11 @@ package com.cos.blog.controller.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,8 +22,9 @@ public class UserApiController {
 	@Autowired
 	private UserService userService;
 	
-	//@Autowired
-	//private AuthenticationManager authenticationManager;
+	@Autowired(required=false)
+	private AuthenticationManager authenticationManager;
+	
 	
 	@PostMapping("/auth/joinProc")
 	public ResponseDto<Integer> save(@RequestBody User user) {
@@ -29,22 +34,21 @@ public class UserApiController {
 		userService.회원가입(user);
 		return new ResponseDto<Integer>(HttpStatus.OK.value(), 1); // 자바오브젝트를 JSON으로 변환해서 리턴(Jackson)
 	}
+
 	@PutMapping("/user")
 	public ResponseDto<Integer> update(@RequestBody User user) { // key=value, x-www-form-urlencoded
 		userService.회원수정(user);
 		// 여기서는 트랜잭션이 종료되기 때문에 DB에 값은 변경이 됐음.
 		// 하지만 세션값은 변경되지 않은 상태이기 때문에 우리가 직접 세션값을 변경해줄 것임.
 		// 세션 등록
-		
-		UserDetails userDetail = principalDetailService.loadUserByUsername(user.getUsername());
-		
-
-		//Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword()));
-		//SecurityContextHolder.getContext().setAuthentication(authentication);
+		System.out.println("pwd : " + user.getPassword());
+		System.out.println("username : " + user.getUsername());
+	
+		Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword()));
+		SecurityContextHolder.getContext().setAuthentication(authentication);
 		
 		return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
 	}
-	
 
 	/*
 	 * @PostMapping("/api/user/login") public ResponseDto<Integer>
